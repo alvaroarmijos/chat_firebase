0ce9e8621e684158286336f1292252e00bf79e44
Alvaro Armijos <56322173+alvaroarmijos@users.noreply.github.com>
2024-09-23 21:02:44 -0500

    Firebase-connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:chat_firebase/app/app.dart';
+import 'package:chat_firebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const App());
+}
